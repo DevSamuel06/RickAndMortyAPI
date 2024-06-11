d55dac2414b21ed1aeae92c99e24834e285138fb
DevSamuel06 <samuelribeiro.filho16@gmail.com>
2024-06-11 08:56:53 -0300

    Ajustando funcionalidades

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 # 🚀 Rick and Morty Vue.js App 🛸
 
-Este é um projeto em Vue.js que consome a API de Rick and Morty para exibir informações sobre personagens da série.
+Este é um projeto em Vue.js que consome a API de Rick and Morty para exibir informações sobre personagens, episódios e locais da série.
 
 ## 📝 Visão Geral
 
 Este projeto foi criado para demonstrar como consumir uma API RESTful em uma aplicação Vue.js. A aplicação permite ao usuário:
 - Visualizar uma lista de personagens 👽
 - Pesquisar personagens por nome 🔍
+- Visualizar detalhes de um personagem específico 📋
+- Navegar entre diferentes páginas de resultados 📄
 
 ## 🛠️ Tecnologias Utilizadas
 
@@ -19,3 +21,8 @@ Este projeto foi criado para demonstrar como consumir uma API RESTful em uma apl
 
 - Listagem de personagens 📝
 - Pesquisa de personagens 🔍
+- Paginação 📑
+- Exibição de detalhes dos personagens 👤
+
+
+
--- scripts.js
+++ scripts.js
@@ -2,26 +2,40 @@ new Vue({
     el: '#app',
     data: {
         characters: [],
-        searchQuery: ''
+        searchQuery: '',
+        isLoading: false,
+        error: null,
+        selectedCharacter: null
     },
     created() {
         this.fetchCharacters();
     },
     methods: {
         async fetchCharacters() {
+            this.isLoading = true;
+            this.error = null;
             const query = this.searchQuery.trim();
             const url = query ? `https://rickandmortyapi.com/api/character/?name=${query}` : 'https://rickandmortyapi.com/api/character';
             try {
                 const response = await fetch(url);
                 if (!response.ok) {
-                    throw new Error('Network response was not ok');
+                    throw new Error('Erro na rede: ' + response.statusText);
                 }
                 const data = await response.json();
                 this.characters = data.results;
             } catch (error) {
-                console.error('Fetching error:', error);
+                console.error('Erro ao buscar personagens:', error);
+                this.error = 'Ocorreu um erro ao buscar os personagens. Tente novamente mais tarde.';
                 this.characters = [];
+            } finally {
+                this.isLoading = false;
             }
+        },
+        showCharacterDetails(character) {
+            this.selectedCharacter = character;
+        },
+        closeModal() {
+            this.selectedCharacter = null;
         }
     }
 });
